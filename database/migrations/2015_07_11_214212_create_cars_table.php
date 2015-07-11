7d2e832f296f840b8764672bb7af95dcaa5bd4ca
Kian Nejadfard <k.nejadfard@gmail.com>
2015-07-11 18:04:07 -0400

    Added migrations for cars and offers

---
+++ database/migrations/2015_07_11_214212_create_cars_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('make');
+            $table->string('model');
+            $table->integer('year');
+            $table->text('description');
+            $table->integer('hp');
+            $table->integer('mpg');
+            $table->integer('msrp');
+            $table->string('offer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cars');
+    }
+}
