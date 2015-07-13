0ef2fe5598969d502552bfe2f3dcfa4ac703cf92
Kian Nejadfard <k.nejadfard@gmail.com>
2015-07-13 13:20:37 -0400

    Changed from div to bootstrap tables

--- resources/views/car/index.blade.php
+++ resources/views/car/index.blade.php
@@ -4,61 +4,35 @@
 @section('content')
         <div class='row'>
             <div class='col-lg-12'>
-                <div class='row'>
-                    <div class='col-lg-1 tableColumn'>
-                        <b>Make</b>
-                    </div>
-                    <div class='col-lg-1 tableColumn'>
-                        <b>Model</b>
-                    </div>
-                    <div class='col-lg-1 tableColumn'>
-                        <b>Year</b>
-                    </div>
-                    <div class='col-lg-1 tableColumn'>
-                        <b>Description</b>
-                    </div>
-                    <div class='col-lg-1 tableColumn'>
-                        <b>HP</b>
-                    </div>
-                    <div class='col-lg-1 tableColumn'>
-                        <b>MPG</b>
-                    </div>
-                    <div class='col-lg-1 tableColumn'>
-                        <b>MSRP</b>
-                    </div>
-                    <div class='col-lg-2 tableColumn'>
-                        <b>Offer</b>
-                    </div>
-                </div>
+                <table class='table table-hover'>
+                    <thead>
+                        <tr>
+                            <td><b>Make</b></td>
+                            <td><b>Model</b></td>
+                            <td><b>Year</b></td>
+                            <td><b>Description</b></td>
+                            <td><b>HP</b></td>
+                            <td><b>MPG</b></td>
+                            <td><b>MSRP</b></td>
+                            <td><b>Offer</b></td>
+                        </tr>
+                    </thead>
 
-                @foreach($cars as $car)
-                    <div class='row clickable-row' id={{ $car->id }}>
-                        <div class='col-lg-1 tableColumn'>
-                            {{ $car->make }}
-                        </div>
-                        <div class='col-lg-1 tableColumn'>
-                            {{ $car->model }}
-                        </div>
-                        <div class='col-lg-1 tableColumn'>
-                            {{ $car->year }}
-                        </div>
-                        <div class='col-lg-1 tableColumn'>
-                            {{ $car->description }}
-                        </div>
-                        <div class='col-lg-1 tableColumn'>
-                            {{ $car->hp }}
-                        </div>
-                        <div class='col-lg-1 tableColumn'>
-                            {{ $car->mpg }}
-                        </div>
-                        <div class='col-lg-1 tableColumn'>
-                            {{ $car->msrp }}
-                        </div>
-                        <div class='col-lg-2 tableColumn'>
-                            {{ $car->offer }}
-                        </div>
-                    </div>
-                @endforeach
+                    <tbody>
+                        @foreach($cars as $car)
+                            <tr class='clickable-row' id={{ $car->id }}>
+                                <td>{{ $car->make }}</td>
+                                <td>{{ $car->model }}</td>
+                                <td>{{ $car->year }}</td>
+                                <td>{{ $car->description }}</td>
+                                <td>{{ $car->hp }}</td>
+                                <td>{{ $car->mpg }}</td>
+                                <td>{{ $car->msrp }}</td>
+                                <td>{{ $car->offer }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
 @stop
