cdc150ea1358b8f58d376d3ac29079c06c5e431e
Kian Nejadfard <k.nejadfard@gmail.com>
2015-07-16 18:13:18 -0400

    Add button implemented in the items index page

--- resources/views/car/index.blade.php
+++ resources/views/car/index.blade.php
@@ -2,37 +2,46 @@
 
 
 @section('content')
-        <div class='row'>
-            <div class='col-lg-12'>
-                <table class='table table-hover'>
-                    <thead>
-                        <tr>
-                            <td><b>Make</b></td>
-                            <td><b>Model</b></td>
-                            <td><b>Year</b></td>
-                            <td><b>Description</b></td>
-                            <td><b>HP</b></td>
-                            <td><b>MPG</b></td>
-                            <td><b>MSRP</b></td>
-                            <td><b>Offer</b></td>
-                        </tr>
-                    </thead>
 
-                    <tbody>
-                        @foreach($cars as $car)
-                            <tr class='clickable-row' id={{ $car->id }}>
-                                <td>{{ $car->make }}</td>
-                                <td>{{ $car->model }}</td>
-                                <td>{{ $car->year }}</td>
-                                <td>{{ $car->description }}</td>
-                                <td>{{ $car->hp }}</td>
-                                <td>{{ $car->mpg }}</td>
-                                <td>{{ $car->msrp }}</td>
-                                <td>{{ $car->offer }}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+    @if(!Auth::guest())
+        <div class='row'>
+            <div class='col-lg-2'>
+                <a href='car/create' class='btn btn-primary'>Add</a>
             </div>
         </div>
+    @endif
+
+    <div class='row'>
+        <div class='col-lg-12'>
+            <table class='table table-hover'>
+                <thead>
+                    <tr>
+                        <td><b>Make</b></td>
+                        <td><b>Model</b></td>
+                        <td><b>Year</b></td>
+                        <td><b>Description</b></td>
+                        <td><b>HP</b></td>
+                        <td><b>MPG</b></td>
+                        <td><b>MSRP</b></td>
+                        <td><b>Offer</b></td>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach($cars as $car)
+                        <tr class='clickable-row' id={{ $car->id }}>
+                            <td>{{ $car->make }}</td>
+                            <td>{{ $car->model }}</td>
+                            <td>{{ $car->year }}</td>
+                            <td>{{ $car->description }}</td>
+                            <td>{{ $car->hp }}</td>
+                            <td>{{ $car->mpg }}</td>
+                            <td>{{ $car->msrp }}</td>
+                            <td>{{ $car->offer }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @stop
