69c75a38a87fc51aa3a813439c803f2036712db9
Kian Nejadfard <k.nejadfard@gmail.com>
2015-07-22 07:27:55 -0400

    Removed the offers page link which was not implemented

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,7 +1,6 @@
 <ul>
     <li><a href={{ url('/') }}>Home</a></li>
     <li><a href={{ url('/car') }}>Inventory</a></li>
-    <li><a href={{ url('/offer') }}>Offers</a></li>
     @if(Auth::guest())
         <li><a href={{ url('/auth/login') }}>Login</a></li>
     @else
